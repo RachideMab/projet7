7e344e700b3da8e07600a4ea8826c21078bd7743
RachideMab <mabilachrisna@gmail.com>
2023-03-04 00:19:08 +0100

    mise à jour

--- bruteforce.py
+++ bruteforce.py
@@ -1,50 +1,56 @@
 from fichier1 import actions
 
-data = actions('data_Set')
-list_stock = data[0]
-capacity = 500
-val = data[1]
-benefit = data[2]
-n = len(val)
-t = [[-1 for i in range(capacity + 1)] for j in range(n+1)]
 
-def force_brute(val, benefit, capacity, n):
+def force_brute(val, benefit, capacity, n, t):
     """
-    This function makes it possible to produce the result of the best 
+    This function makes it possible to produce the result of the best
     possible benefit with the maximum investment.
     """
-    #base conditions
+    # base conditions
     if n == 0 or capacity == 0:
         return 0
-
     if t[n][capacity] != -1:
         return t[n][capacity]
     # choice diagram code
     if val[n-1] <= capacity:
-        t[n][capacity] = max(benefit[n-1] + force_brute(val, benefit, 
-        capacity - int(val[n-1]), n-1), force_brute(val, benefit, capacity, n-1))
-        return t[n][capacity]  
+        t[n][capacity] = max(benefit[n-1] + force_brute(val, benefit,
+                             capacity - int(val[n-1]), n-1, t),
+                             force_brute(val, benefit, capacity, n-1, t))
+        return t[n][capacity]
     elif val[n-1] > capacity:
-        t[n][capacity] = force_brute(val, benefit, capacity, n-1)
-        return  t[n][capacity]
+        t[n][capacity] = force_brute(val, benefit, capacity, n-1, t)
+        return t[n][capacity]
 
-print(force_brute(val, benefit, capacity, n))
 
-res = t[n][capacity]
-w = capacity
-"""
-This loop allows you to list all the actions purchased, 
-to obtain the best benefit.
-"""
-for i in range(n, 0, -1):
-    if res <= 0:
-        break
-    if res == t[i - 1][w]:
-        continue
-    else:        
-        res = res - benefit[i - 1]
-        
-        if res >= 0:
-            w = w - int(val[i - 1])
-            print(list_stock[i - 1], val[i - 1], benefit[i - 1], w)
-print(capacity-w)
+def forcebrute(val, benefit, capacity):
+    n = len(val)
+    t = [[-1 for i in range(capacity + 1)] for j in range(n+1)]
+    force_brute(val, benefit, capacity, n, t)
+    return t
+
+
+if __name__ == "__main__":
+    data = actions('data_Set')
+    list_stock = data[0]
+    capacity = 500
+    val = data[1]
+    benefit = data[2]
+    t = forcebrute(val, benefit, capacity)
+    res = t[-1][capacity]
+    print(res)
+    w = capacity
+    """
+    This loop allows you to list all the actions purchased,
+    to obtain the best benefit.
+    """
+    for i in range(len(val), 0, -1):
+        if res <= 0:
+            break
+        if res == t[i - 1][w]:
+            continue
+        else:
+            res = res - benefit[i - 1]
+            if res >= 0:
+                w = w - int(val[i - 1])
+                print(list_stock[i - 1], val[i - 1], benefit[i - 1], w)
+    print(capacity-w)
--- optimized.py
+++ optimized.py
@@ -1,6 +1,5 @@
 from tqdm import tqdm
 from fichier import actions
-# from fichier1 import actions
 
 capacity = 500*100
 data = actions('data_Set')
@@ -9,16 +8,16 @@ val = data[1]
 benefice = data[2]
 n = len(val)
 
+
 def algo_optimized(capacity, benefice, val, n, list_stock):
     K = [[0 for x in range(capacity + 1)] for x in range(n + 1)]
     for i in tqdm(range(n + 1)):
-        for w in range(capacity + 1): 
+        for w in range(capacity + 1):
             if i == 0 or w == 0:
                 K[i][w] = 0
             elif val[i-1] <= w:
-                
-                K[i][w] = max(benefice[i-1] + K[i-1][w-int(val[i-1])], K[i-1][w])
-                
+                K[i][w] = max(benefice[i-1] + K[i-1][w-int(val[i-1])],
+                              K[i-1][w])
             else:
                 K[i][w] = K[i-1][w]
     liste_action = []
@@ -28,10 +27,8 @@ def algo_optimized(capacity, benefice, val, n, list_stock):
         if K[y][m] == benefice[y-1] + K[y-1][m-int(val[i-1])]:
             liste_action.append(list_stock[y-1])
             m = m - int(val[i-1])
-        y = y - 1    
-
+        y = y - 1
     return K[n][capacity], liste_action
 
+
 print(algo_optimized(capacity, benefice, val, n, list_stock))
-
-
